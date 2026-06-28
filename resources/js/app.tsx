1e53dcb3593a5b122093c6c61bd13364d2cdaa06
Abdo <Qeysabaxa11@gmail.com>
2026-06-28 20:18:03 +0300

    Update:- app render updated

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,9 +1,18 @@
 import { createInertiaApp } from '@inertiajs/react';
+import { createRoot } from 'react-dom/client';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
+    resolve: (name) => {
+        const pages = import.meta.glob('./pages/**/*.tsx', { eager: true });
+        return pages[`./pages/${name}.tsx`].default;
+    },
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+        root.render(<App {...props} />);
+    },
     progress: {
         color: '#4B5563',
     },
